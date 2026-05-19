43e87a9e0df73bbaefec3cb1aa6954d3fc97d13b
choihahye <coolage512@gmail.com>
2026-05-19 22:38:32 +0900

    [FE] 테스트 진행 페이지 더미데이터 맵핑

--- app/(main)/test/[test_id]/page.tsx
+++ app/(main)/test/[test_id]/page.tsx
@@ -5,7 +5,8 @@ import Badge from "@/components/ui/Badge";
 import ArrowIcon from "@/components/icon/ArrowIcon";
 import ViewAllButton from "@/components/ui/ViewAllLink";
 import ArrowIcon2 from "@/components/icon/ArrowIcon2";
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
 // export const metadata = {
 //   title: "테스트 상세",
 // };
@@ -16,28 +17,32 @@ const questions = [
     testId: 4,
     testNumbering: 1,
     question: "zzzzzzzzzzz",
-    answer1: "zzzzzz",
-    answer1Scale: "",
-    answer2: "zzzzzzzzz",
-    answer2Scale: "",
-    answer3: "zzzzzzzz",
-    answer3Scale: "",
-    answer4: "zzzzzzzzz",
-    answer4Scale: "",
+    answer: [
+      {
+        content: "answer1",
+        scale: "5",
+      },
+      {
+        content: "answer2",
+        scale: "3",
+      },
+    ],
   },
   {
     contentId: 5,
     testId: 4,
     testNumbering: 1,
-    question: "zzzzzzzzzzz",
-    answer1: "zzzzzz",
-    answer1Scale: "",
-    answer2: "zzzzzzzzz",
-    answer2Scale: "",
-    answer3: "zzzzzzzz",
-    answer3Scale: "",
-    answer4: "zzzzzzzzz",
-    answer4Scale: "",
+    question: "zzzwdwzzzz",
+    answer: [
+      {
+        content: "answer122",
+        scale: "5",
+      },
+      {
+        content: "answer233",
+        scale: "3",
+      },
+    ],
   },
 ];
 const data = {
@@ -77,48 +82,83 @@ export default function Page() {
 }
 
 export function PlayTest() {
+  const [num, setNum] = useState(0);
+  const [progress, setProgress] = useState((num / questions.length) * 100);
+
+  useEffect(() => {
+    setProgress((num / questions.length) * 100);
+  }, [num]);
+
+  const [arr, setArr] = useState<string[]>([]);
+  const addAnswer = (item: string) => {
+    console.log(num);
+    if (num + 1 < questions.length) {
+      setArr((prev: string[]) => {
+        const newArr = [...prev, item];
+        console.log(newArr);
+        return newArr;
+      });
+      setNum((prev: number) => prev + 1);
+    } else {
+      // 결과 api 호출!!!!!!!
+      alert("마지막!");
+    }
+  };
+  const goBack = () => {
+    if (num == 0) {
+      // 작업필요
+      alert("hihi");
+    }
+    setNum((prev: number) => prev - 1);
+    setArr((prev: string[]) => prev.slice(0, -1));
+  };
   return (
     <>
       <div className="mb-10 text-right">
-        <Badge size="sm">03/10</Badge>
+        <Badge size="sm">
+          {num + 1}/{questions.length}
+        </Badge>
         <div className="relative w-full h-3 mt-3 rounded-button bg-background overflow-hidden">
-          <span className="inline-block w-[10%] bg-border-main h-full absolute left-0"></span>
+          <span
+            className={`inline-block w-[${progress}%] bg-border-main h-full absolute left-0`}
+          ></span>
         </div>
         {/* progressbar */}
       </div>
-      <h2 className="text-h4 mb-10">출근룩?</h2>
-      <div className="mb-10">
-        <button
-          type="button"
-          className="group flex p-6 items-center gap-4 border-1 border-border-sub bg-background rounded-button mb-2"
-        >
-          <span className="flex items-center justify-center w-10 h-10 rounded-button bg-primary text-white text-body-m">
-            A
-          </span>
-          <p className="flex-1 min-h-10 text-left"> 질문1</p>
-          <div className="relative group-hover:left-1">
-            <ArrowIcon width={9} height={13.6} className="text-primary" />
-          </div>
-        </button>
-        <div className="group flex p-6 items-center gap-4 border-1 border-border-sub bg-background rounded-button">
-          <span className="flex items-center justify-center w-10 h-10 rounded-button bg-primary text-white text-body-m">
-            B
-          </span>
-          <p className="flex-1 min-h-10 text-left">일정에 따라 룩도 다르게</p>
-          <div className="relative group-hover:left-1">
-            <ArrowIcon width={9} height={13.6} className="text-primary " />
-          </div>
+      <>
+        <h2 className="text-h4 mb-10">{questions[num].question}</h2>
+        <div className="mb-10">
+          {questions[num].answer.map((item, index) => (
+            <button
+              onClick={() => addAnswer(item.scale)}
+              type="button"
+              className="group flex p-6 items-center gap-4 border-1 border-border-sub bg-background rounded-button mb-2"
+            >
+              <span className="flex items-center justify-center w-10 h-10 rounded-button bg-primary text-white text-body-m">
+                {index + 1}
+              </span>
+              <p className="flex-1 min-h-10 text-left"> {item.content} </p>
+
+              <div className="relative group-hover:left-1">
+                <ArrowIcon width={9} height={13.6} className="text-primary" />
+              </div>
+            </button>
+          ))}
         </div>
-      </div>
+      </>
       <hr className="my-6 border-secondary-light" />
-      <PrevButton />
+      <PrevButton onClick={goBack} />
     </>
   );
 }
-
-export function PrevButton() {
+type ButtonProps = React.ComponentProps<"button">;
+export function PrevButton({ children, ...props }: ButtonProps) {
   return (
-    <button className="group flex items-center gap-1 p-[10px] text-text-sub text-body-m">
+    <button
+      type="button"
+      {...props}
+      className="group flex items-center gap-1 p-[10px] text-text-sub text-body-m"
+    >
       <span className="group-hover:right-1 relative transition-all">
         <ArrowIcon2 className="text-text-sub rotate-180" size={10} />
       </span>

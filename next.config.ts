44e8258149aae162662f44345b42f5ae10641d92
DESKTOP-F0M2E3D\forwiz014 <coolage512@gmail.com>
2026-08-11 13:20:53 +0900

    롤백

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // 렌더링을 차단하는 Critical CSS를 HTML 내부에 인라인화하여 LCP/FCP를 개선합니다.
-    optimizeCss: true,
-  },
 };
 
 export default nextConfig;
